dcb8be5c42ba322ce047ee67073e93912d9c5d51
PATIANT_X <ngwenyathabani080@gmail.com>
2024-02-23 22:25:19 +0200

    update

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,7 +1,6 @@
 import asyncHandler from '../middleware/asyncHandler.js';
 import Order from '../models/orderModel.js';
 import Product from '../models/productModel.js';
-import User from '../models/userModel.js';
 import SendEmail from '../utils/SendEmail.js';
 import { calcPrices } from '../utils/calcPrices.js';
 import { OrderConfirmationContent } from '../utils/emailContents.js';
