0a483d743381e591d279a4c6bbaad8eecc504baa
PATIANT_X <ngwenyathabani080@gmail.com>
2024-04-05 15:11:42 +0200

    notification update

--- frontend/src/hooks/useListenNewOrders.js
+++ frontend/src/hooks/useListenNewOrders.js
@@ -1,34 +1,10 @@
 import { useSocketContext } from '../context/SocketContext.js';
 import { useEffect } from 'react';
-// import addNotification from 'react-push-notification';
 
 const useListenNewOrders = () => {
   const { socket } = useSocketContext();
 
-  // const notify = () => {
-  //   addNotification({
-  //     title: '5TygaEats',
-  //     message: 'There is a new order. Please start Preparing',
-  //     duration: 5000,
-  //     native: true,
-  //   });
-  // };
-  // const notifyThabani = () => {
-  //   addNotification({
-  //     title: 'Thabani',
-  //     message: 'Please tell Thabani It worked',
-  //     duration: 5000,
-  //     native: true,
-  //   });
-  // };
-  // const notifyUser = () => {
-  //   addNotification({
-  //     title: '5TygaEats',
-  //     message: 'The driver has arrived',
-  //     duration: 5000,
-  //     native: true,
-  //   });
-  // };
+ 
 
   useEffect(() => {
     // Check if the browser supports notifications
@@ -49,10 +25,7 @@ const useListenNewOrders = () => {
               body: 'It Worked Please tell Thabani',
             });
             notification();
-            // if (newOrder) {
-            //   notifyThabani();
-            // }
-
+           
             // Automatically close the notification after a few seconds
             setTimeout(() => notification.close(), 5000);
           }
@@ -62,10 +35,7 @@ const useListenNewOrders = () => {
           const notification = new Notification('5TygaEats', {
             body: 'There is a new order. Please start Preparing',
           });
-          // if (newOrder) {
-          //   notify();
-          // }
-
+         
           // Automatically close the notification after a few seconds
           setTimeout(() => notification.close(), 5000);
         }
@@ -77,10 +47,7 @@ const useListenNewOrders = () => {
           const notification = new Notification('5TygaEats', {
             body: 'The driver has Arrived',
           });
-          // if (arrivedMessage) {
-          //   notifyUser();
-          // }
-
+          
           // Automatically close the notification after a few seconds
          setTimeout(() => notification.close(), 5000);
         }
